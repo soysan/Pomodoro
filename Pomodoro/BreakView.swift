e308273d7967e2deaf77233fc70bd355ebf4ef57
Masayuki <soy2.san@gmail.com>
2021-03-22 21:27:44 +0900

    Timer function at StudyView

--- Pomodoro/BreakView.swift
+++ Pomodoro/BreakView.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 class BreakView: UIViewController {
+    
     // MARK: - Properties
+    
+    let colors = Colors()
+    
+    let sectionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Break"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(goBack(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     // MARK: - LifeCycle
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = colors.yellow
+        
+        view.addSubview(sectionLabel)
+        sectionLabel.textColor = colors.white
+        
+        view.addSubview(resetButton)
+        resetButton.backgroundColor = colors.lightBlue
+        resetButton.tintColor = colors.white
+        
+        sectionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        sectionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        resetButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        resetButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        resetButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+    
     // MARK: - Actions
+    
+    @objc
+    func goBack(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
     // MARK: - Helpers
 
--- Pomodoro/StudyView.swift
+++ Pomodoro/StudyView.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class StudyView: UIViewController {
     
+    // MARK: - Properties
+    
     let colors = Colors()
     
-    let backButton: UIButton = {
+    var timer = Timer()
+    var currentTime = 5
+    
+    let sectionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Concentration"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+       let label = UILabel()
+        label.text = ""
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let resetButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Back", for: .normal)
+        button.setTitle("Reset", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.layer.cornerRadius = 10
         button.addTarget(self, action: #selector(goBack(_:)), for: .touchUpInside)
@@ -21,22 +42,90 @@ class StudyView: UIViewController {
         return button
     }()
     
+    let startButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 100)
+        button.setTitle("Start", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(startTimer(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = colors.deepBlue
-        view.addSubview(backButton)
-        backButton.backgroundColor = colors.lightBlue
-        backButton.tintColor = colors.white
+        updateTime()
         
-        backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
-        backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        view.backgroundColor = colors.deepBlue
+        
+        view.addSubview(sectionLabel)
+        sectionLabel.textColor = colors.white
+        
+        view.addSubview(timeLabel)
+        timeLabel.text = String(currentTime)
+        timeLabel.textColor = colors.white
+        
+        view.addSubview(resetButton)
+        resetButton.backgroundColor = colors.lightBlue
+        resetButton.tintColor = colors.white
+        
+        timeLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        timeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        view.addSubview(startButton)
+        startButton.backgroundColor = colors.lightBlue
+        startButton.tintColor = colors.white
+        startButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        startButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        startButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        sectionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        sectionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        resetButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        resetButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        resetButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - Actions
+    
+    @objc
+    func countDown() {
+        
+    }
+
     @objc
     func goBack(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc
+    func startTimer(_ sender: UIButton) {
+        let nextView = BreakView()
+        nextView.modalPresentationStyle = .fullScreen
+        nextView.modalTransitionStyle = .crossDissolve
+        present(nextView, animated: true, completion: nil)
+    }
+    // MARK: - Helpers
+    
+    func updateTime() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.currentTime -= 1
+            self.timeLabel.text = String(self.currentTime)
+            
+            if self.currentTime == 0 {
+                let nextView = BreakView()
+                nextView.modalPresentationStyle = .fullScreen
+                nextView.modalTransitionStyle = .crossDissolve
+                self.present(nextView, animated: true, completion: nil)
+            }
+        })
     }
 }
